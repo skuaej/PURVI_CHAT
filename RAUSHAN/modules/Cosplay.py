f8a4de044c86fb600cff02a891f1db9f67dba599
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-08-24 12:49:21 +0530

    Update Cosplay.py

--- RAUSHAN/modules/Cosplay.py
+++ RAUSHAN/modules/Cosplay.py
@@ -15,7 +15,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from RAUSHAN import dev
+from RAUSHAN import Dev as app 
 
 SAUCENAO_API_KEY = "27ab8b6f0a9e67ce164e67dcbecc45a362b538ef"
 SAUCENAO_API = "https://saucenao.com/search.php"
